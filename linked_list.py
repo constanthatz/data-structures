0c231c18f3ccd10c752f2cca63fdec8cc283f9ba
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-04 12:22:06 -0800

    Constantine: completed verison 1 of insert function which inserts node at head and assigns previous head as next for the new node.

--- linked_list.py
+++ linked_list.py
@@ -1,25 +1,43 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+
 class Node(object):
 
-    def __init__(self, val):
-        self.val = val
+    def __init__(self, value):
+        self._val = value
         self._next = None
 
-        @property
-        def next(self):
-            return self._next
+    @property
+    def next(self):
+        return self._next
 
-        @next.setter
-        def next(self, value):
-            self._next = value
+    @next.setter
+    def next(self, value):
+        self._next = value
+
+    @property
+    def val(self):
+        return self._val
 
 
 class LinkedList(object):
 
-    def __init__(self, name):
-        self.head = None
+    def __init__(self):
+        self._head = None
         self.second = None
 
     def insert(self, val):
-        self.second, self.head = self.head, Node(val)
-        self.head.next = self.second
+        self.second, self._head = self._head, Node(val)
+        self._head.next = self.second
 
+    @property
+    def head(self):
+        return self._head
+
+
+l = LinkedList()
+l.insert('Nick')
+l.insert('Constantine')
+print(l.head.val)
+print(l.head.next.val)

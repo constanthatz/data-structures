75c4effcd49132793611069a649763dfb8c70373
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-04 12:04:01 -0800

    Constantine: add basic class definitions.

---
+++ linked_list.py
@@ -0,0 +1,11 @@
+class Node(object):
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class LinkedList(object):
+
+    def __init__(self, name):
+        self.head = None

bc8273b261d2aed0620b4407b69315bb96c09687
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-04 12:13:07 -0800

    Constantine: add insert method.

--- linked_list.py
+++ linked_list.py
@@ -2,10 +2,23 @@ class Node(object):
 
     def __init__(self, val):
         self.val = val
-        self.next = None
+        self._next = None
+
+        @property
+        def next(self):
+            return self._next
+
+        @next.setter
+        def name(self, value):
+            self._next = value
 
 
 class LinkedList(object):
 
     def __init__(self, name):
         self.head = None
+        self.active = None
+
+    def insert(self, val):
+        self.active, self.head = self.head, Node(val)
+        self.head.next = self.active

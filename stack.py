8ffb33662dade7d0cf49c428e7e578e32473c2c1
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-05 13:06:06 -0800

    add docstrings and comments to stack module.

--- stack.py
+++ stack.py
@@ -4,23 +4,27 @@ from __future__ import unicode_literals
 
 
 class Element(object):
-
+    ''' Create data element with default value and previous pointer. '''
     def __init__(self, value=None, previous=None):
+        ''' Value and previous pointer default to none. '''
         self.val = value
         self.previous = previous
 
 
 class Stack(object):
-
+    ''' Create an empty stack. '''
     def __init__(self):
         self.top = None
 
     def push(self, element):
+        ''' Add data element to the top of stack. '''
         self.top, self.top.previous = element, self.top
 
     def pop(self):
+        ''' Remove top element from stack. Reassign and reassign top data
+            element. '''
         if not self.top:
-            ''' No element to pop is considered a ValueError '''
+            # If no data element to pop it is considered a ValueError.
             raise ValueError
         else:
             val = self.top.val

82ffe6be6490b0de42b808667b7b53c05e30cede
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-05 12:28:42 -0800

    first try at push and pop methods.

---
+++ stack.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+class Element(object):
+
+    def __init__(self, value=None, previous=None):
+        self.val = value
+        self.previous = next
+
+
+class Stack(object):
+
+    def __init__(self):
+        self.top = None
+
+    def push(self, val):
+        self.top, self.top.previous = Element(val, self.top)
+
+    def pop(self):
+        if not self.top:
+            ''' No element to pop is considered a ValueError '''
+            raise ValueError
+        else:
+            val = self.top.val
+            self.top = self.top.previous
+            return val

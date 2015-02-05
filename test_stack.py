26de6c5decac3345dee470a0968926a65d3497b9
Nick Becker <nbeck90@uw.edu>
2015-02-05 12:56:45 -0800

    Add test for pop and adjust element init test

--- test_stack.py
+++ test_stack.py
@@ -6,10 +6,10 @@ from stack import Stack
 def test_element_init():
     n = Element()
     assert n.val is None
-    assert n.next is None
-    n = Element(3)
-    assert n.val == 3
-    assert n.next is None
+    assert n.previous is None
+    m = Element(3)
+    assert m.val == 3
+    assert m.previous is None
 
 
 def test_stack_init():
@@ -30,7 +30,16 @@ def test_stack_push():
     assert l.top.previous.previous is None
 
 
-# def test_stack_pop():
-#     l = stack()
-#     a = l.pop()
-#     assert a is None
+def test_stack_pop():
+    l = Stack()
+    e = Element(10)
+    l.push(e)
+    f = Element("String")
+    l.push(f)
+    assert l.pop() == f.val
+    assert l.top == e
+    assert l.top.previous is None
+    assert l.pop() == e.val
+    assert l.top is None
+    with pytest.raises(ValueError):
+        l.pop()

74dee1d09fdc09f93af3d15286336d7face4ba08
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-06 17:51:02 -0800

    add test file for proper_parens.

---
+++ test_proper_parens.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+from proper_parens import check_statement
+
+
+def test_check_statement():
+
+    # Edge cases of strings of length one
+    value = ")"
+    assert check_statement(value) == -1
+
+    value = "("
+    assert check_statement(value) == 1
+
+    # Edge cases of strings of length two
+    value = "()"
+    assert check_statement(value) == 0
+
+    # 'Balanced' but broken
+    value = ")("
+    assert check_statement(value) == -1
+
+    # Broken beginnning, middle, and end
+    value = ")()"
+    assert check_statement(value) == -1
+
+    value = "())()"
+    assert check_statement(value) == -1
+
+    value = "())"
+    assert check_statement(value) == -1
+
+    # Open beginnning, middle, and end
+    value = "(()"
+    assert check_statement(value) == 1
+
+    value = "()(()"
+    assert check_statement(value) == 1
+
+    value = "()("
+    assert check_statement(value) == 1

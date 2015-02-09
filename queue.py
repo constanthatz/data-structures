5712b44c6cd3cea8bdb7a560057cb19884402f90
Nick Becker <nbeck90@uw.edu>
2015-02-09 13:27:48 -0800

    fix enqueue to properly reassign back and front and add pointers for new element and previous back element

--- queue.py
+++ queue.py
@@ -19,21 +19,57 @@ class Queue(object):
 
     def enqueue(self, value):
         ''' Add data element to the back of queue. '''
-        self.back.behind = Element(value, self.back)
-        self.back = self.back.behind
+        self.back = Element(value, self.back)
+
         if not self.front:
             self.front = self.back
+        else:
+            self.back.ahead.behind = self.back
 
-    def dequeue(self):
-        ''' Remove front element from front of queue. Reassign front data
-            element and return value of dequeued element. '''
-        try:
-            val = self.front.val
-        except AttributeError:
-            ''' Mimic error message from list '''
-            raise IndexError("dequeue from empty Stack")
-        self.front = self.front.previous
-        return val
+    # def dequeue(self):
+    #     ''' Remove front element from front of queue. Reassign front data
+    #         element and return value of dequeued element. '''
+    #     try:
+    #         val = self.front.val
+    #     except AttributeError:
+    #         ''' Mimic error message from list '''
+    #         raise IndexError("dequeue from empty Stack")
+    #     self.front = self.front.previous
+    #     return val
 
-    def size(self):
-        return size
+    # def size(self):
+    #     return size
+
+
+x = Queue()
+
+x.enqueue("A")
+print(x.front)
+print(x.back)
+print(x.front.val)
+print(x.back.val)
+x.enqueue("B")
+print(x.front)
+print(x.back)
+print(x.front.val)
+print(x.back.val)
+x.enqueue("C")
+print(x.front)
+print(x.back)
+print(x.front.val)
+print(x.back.val)
+print(x.back.ahead.val)
+print(x.front.behind.val)
+print(x.front.ahead)
+print(x.back.behind)
+print(x.back.ahead.behind.val)
+x.enqueue("D")
+print(x.front)
+print(x.back)
+print(x.front.val)
+print(x.back.val)
+print(x.back.ahead.val)
+print(x.front.behind.val)
+print(x.front.ahead)
+print(x.back.behind)
+print(x.back.ahead.behind.val)

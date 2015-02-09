3d57f989667928d2219cab96dc6c5aac593dee4f
Nick Becker <nbeck90@uw.edu>
2015-02-09 13:31:56 -0800

    Add dequeue function

--- queue.py
+++ queue.py
@@ -26,16 +26,16 @@ class Queue(object):
         else:
             self.back.ahead.behind = self.back
 
-    # def dequeue(self):
-    #     ''' Remove front element from front of queue. Reassign front data
-    #         element and return value of dequeued element. '''
-    #     try:
-    #         val = self.front.val
-    #     except AttributeError:
-    #         ''' Mimic error message from list '''
-    #         raise IndexError("dequeue from empty Stack")
-    #     self.front = self.front.previous
-    #     return val
+    def dequeue(self):
+        ''' Remove front element from front of queue. Reassign front data
+            element and return value of dequeued element. '''
+        try:
+            val = self.front.val
+        except AttributeError:
+            ''' Mimic error message from list '''
+            raise IndexError("dequeue from empty Stack")
+        self.front = self.front.behind
+        return val
 
     # def size(self):
     #     return size
@@ -43,33 +43,47 @@ class Queue(object):
 
 x = Queue()
 
-x.enqueue("A")
-print(x.front)
-print(x.back)
-print(x.front.val)
-print(x.back.val)
-x.enqueue("B")
-print(x.front)
-print(x.back)
-print(x.front.val)
-print(x.back.val)
-x.enqueue("C")
-print(x.front)
-print(x.back)
-print(x.front.val)
-print(x.back.val)
-print(x.back.ahead.val)
-print(x.front.behind.val)
-print(x.front.ahead)
-print(x.back.behind)
-print(x.back.ahead.behind.val)
-x.enqueue("D")
-print(x.front)
-print(x.back)
-print(x.front.val)
-print(x.back.val)
-print(x.back.ahead.val)
-print(x.front.behind.val)
-print(x.front.ahead)
-print(x.back.behind)
-print(x.back.ahead.behind.val)
+# x.enqueue("A")
+# print(x.front)
+# print(x.back)
+# print(x.front.val)
+# print(x.back.val)
+# x.enqueue("B")
+# print(x.front)
+# print(x.back)
+# print(x.front.val)
+# print(x.back.val)
+# x.enqueue("C")
+# print(x.front)
+# print(x.back)
+# print(x.front.val)
+# print(x.back.val)
+# print(x.back.ahead.val)
+# print(x.front.behind.val)
+# print(x.front.ahead)
+# print(x.back.behind)
+# print(x.back.ahead.behind.val)
+# x.enqueue("D")
+# print(x.front)
+# print(x.back)
+# print(x.front.val)
+# print(x.back.val)
+# print(x.back.ahead.val)
+# print(x.front.behind.val)
+# print(x.front.ahead)
+# print(x.back.behind)
+# print(x.back.ahead.behind.val)
+
+# x.enqueue("A")
+# x.enqueue("B")
+# x.enqueue("C")
+# x.enqueue("D")
+# print(x.dequeue())
+
+# print(x.dequeue())
+
+# print(x.dequeue())
+
+# print(x.dequeue())
+
+

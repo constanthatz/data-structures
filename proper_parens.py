8113907fc21d2a5ba42363bbf0544061da27d74e
Nick Becker <nbeck90@uw.edu>
2015-02-09 09:07:30 -0800

    Finalized version

--- proper_parens.py
+++ proper_parens.py
@@ -3,29 +3,16 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 
-def safe_input(prompt):
-    """Return user input after catching KeyboardInterrupt and EOFError"""
-    try:
-        reply = raw_input(prompt)
-    except (EOFError, KeyboardInterrupt):
-        quit()
-    else:
-        return reply.decode('utf-8')  # Convert input to unicode
-
-prompt = "Input a Lisp style statement '(test)': "
-reply = safe_input(prompt)
-
-
-def check_statement(value):
+def check_statement1(value):
     output = 0
     while output >= 0:
         for item in value:
-                if item == ")":
-                    output -= 1
-                    if output == -1:
-                        return -1
-                elif item == "(":
-                    output += 1
+            if item == ")":
+                output -= 1
+                if output == -1:
+                    return -1
+            elif item == "(":
+                output += 1
         if output == 0:
             return 0
         elif output > 1:

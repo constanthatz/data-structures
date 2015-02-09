5f9790a62d100f985465118dfc5ab4ca48c1bafb
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-09 12:47:30 -0800

    define enqueue function to add element to back of queue.

--- queue.py
+++ queue.py
@@ -3,22 +3,22 @@ from __future__ import print_function
 
 
 class Element(object):
-    ''' Create data element with default value and previous pointer. '''
+    ''' Create data element with default value and next pointer. '''
     def __init__(self, value, next=None):
-        ''' Value and previous pointer default to none. '''
+        ''' Value and next pointer default to none. '''
         self.val = value
         self.next = next
 
 
 class Queue(object):
-    ''' Create an empty stack. '''
+    ''' Create an empty queue. '''
     def __init__(self):
         self.front = None
         self.back = None
 
     def enqueue(self, value):
-        ''' Add data element to the top of stack. '''
-        self.top = Element(value, self.top)
+        ''' Add data element to the back of queue. '''
+        self.back = Element(value, self.back)
 
     def dequeue(self):
         ''' Remove top element from stack. Reassign and reassign top data

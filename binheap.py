3fca8907e9a7d316d699731d0207c0673e6dc56f
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-11 12:39:47 -0800

    initialize binheap class with ampty list.

--- binheap.py
+++ binheap.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import unicode_literals
+
+
+class Binheap(object):
+    ''' Create an empty heap. '''
+    def __init__(self):
+        self.list = []

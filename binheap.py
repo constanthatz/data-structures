5baf8653b69985aa7a251ff7130d46dad455ba3b
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-11 13:22:08 -0800

    add parent and child find methods.

--- binheap.py
+++ binheap.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import unicode_literals
+from __future__ import division
 
 
 class Binheap(object):
@@ -10,7 +11,26 @@ class Binheap(object):
 
     def push(self, value):
         self.binlist.append[value]
+        self.__promote()
         return
+
+    def __promote(self):
+        return
+
+
+    def __parent(self, index):
+        parent = [(index-1)//2, self.binlist[(index-1)//2]]
+
+
+    def __children(self, index):
+        parent = [(2*index+1, self.binlist[2*index+1]), ((2*index+2, self.binlist[2*index+2]))]
+        return
+
+
+    def __swap():
+        return
+
+
 
     def pop(self):
         top = self.binlist[0]
@@ -29,9 +49,18 @@ class Binheap(object):
         child1 = 2*empty_bin+1
         child2 = 2*empty_bin+2
 
-        if self.binlist[child2] >= self.binlist[child1]:
-            self.binlist[empty_bin] = self.binlist[child2]
-        else:
-            self.binlist[empty_bin] = self.binlist[child1]
-        return
+        try:
+            while self.binlist[child2] or self.binlist[child1]:
+                if self.binlist[child2] >= self.binlist[child1]:
+                    self.binlist[empty_bin] = self.binlist[child2]
+                    empty_bin = child2
+                else:
+                    self.binlist[empty_bin] = self.binlist[child1]
+                    empty_bin = child1
 
+                child1 = 2*empty_bin+1
+                child2 = 2*empty_bin+2
+                return
+
+        except IndexError:
+            return

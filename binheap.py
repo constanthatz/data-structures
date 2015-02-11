107dc4f23e1d73ff88a22b28b3174287c4aeaf5a
Nick Becker <nbeck90@uw.edu>
2015-02-11 13:40:14 -0800

    Adjusted whitespace around operators

--- binheap.py
+++ binheap.py
@@ -23,11 +23,11 @@ class Binheap(object):
 
 
     def __parent(self, index):
-        parent = [(index-1)//2, self.binlist[(index-1)//2]]
+        parent = [(index - 1)//2, self.binlist[(index-1)//2]]
 
 
     def __children(self, index):
-        children = [(2*index+1, self.binlist[2*index+1]), ((2*index+2, self.binlist[2*index+2]))]
+        children = [(2 * index + 1, self.binlist[2*index+1]), ((2*index+2, self.binlist[2*index+2]))]
         return
 
 
@@ -36,36 +36,35 @@ class Binheap(object):
         return
 
 
+    # def pop(self):
+    #     top = self.binlist[0]
 
-    def pop(self):
-        top = self.binlist[0]
+    #     if self.binlist[2] >= self.binlist[1]:
+    #         self.binlist[0] = self.binlist[2]
+    #         empty_bin = 2
+    #     else:
+    #         self.binlist[0] = self.binlist[1]
+    #         empty_bin = 1
 
-        if self.binlist[2] >= self.binlist[1]:
-            self.binlist[0] = self.binlist[2]
-            empty_bin = 2
-        else:
-            self.binlist[0] = self.binlist[1]
-            empty_bin = 1
+    #     self.__resort(empty_bin)
+    #     return top
 
-        self.__resort(empty_bin)
-        return top
+    # def __resort(self, empty_bin):
+    #     child1 = 2*empty_bin+1
+    #     child2 = 2*empty_bin+2
 
-    def __resort(self, empty_bin):
-        child1 = 2*empty_bin+1
-        child2 = 2*empty_bin+2
+    #     try:
+    #         while self.binlist[child2] or self.binlist[child1]:
+    #             if self.binlist[child2] >= self.binlist[child1]:
+    #                 self.binlist[empty_bin] = self.binlist[child2]
+    #                 empty_bin = child2
+    #             else:
+    #                 self.binlist[empty_bin] = self.binlist[child1]
+    #                 empty_bin = child1
 
-        try:
-            while self.binlist[child2] or self.binlist[child1]:
-                if self.binlist[child2] >= self.binlist[child1]:
-                    self.binlist[empty_bin] = self.binlist[child2]
-                    empty_bin = child2
-                else:
-                    self.binlist[empty_bin] = self.binlist[child1]
-                    empty_bin = child1
+    #             child1 = 2*empty_bin+1
+    #             child2 = 2*empty_bin+2
+    #             return
 
-                child1 = 2*empty_bin+1
-                child2 = 2*empty_bin+2
-                return
-
-        except IndexError:
-            return
+    #     except IndexError:
+    #         return

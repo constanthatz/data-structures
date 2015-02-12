19cd41f03a6c9fbf0cddcefc8d36b371463347c3
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-12 13:57:01 -0800

    fix tests.

--- test_priorityq.py
+++ test_priorityq.py
@@ -2,6 +2,15 @@ from priorityq import Element
 from priorityq import Priorityq
 
 
+def que(queue):
+    que = []
+    current = queue.back
+    while current:
+        que.append(current.val)
+        current = current.ahead
+    return que
+
+
 def test_element_init():
     m = Element(3, 3)
     assert m.val == 3
@@ -19,21 +28,36 @@ def test_prioq_init():
 def test_priorityq_prio():
     l = Priorityq()
     l.insert("Nick", 1)
+    ql = que(l)
     assert l.front.val == "Nick"
     assert l.back.val == "Nick"
+    assert ql == ["Nick"]
 
     l.insert("Constantine", 1)
+    ql = que(l)
     assert l.front.val == "Nick"
     assert l.back.val == "Constantine"
+    assert ql == ["Constantine", "Nick"]
 
     l.insert("Mark", 2)
+    ql = que(l)
     assert l.front.val == "Mark"
     assert l.back.val == "Constantine"
+    assert l.back.ahead.val == "Nick"
+    assert ql == ["Constantine", "Nick",  "Mark"]
+
 
     l.insert("Henry", 2)
+    ql = que(l)
     assert l.front.val == "Mark"
     assert l.front.behind.val == "Henry"
     assert l.back.val == "Constantine"
+    assert ql == ["Constantine", "Nick", "Henry", "Mark"]
 
     l.insert("Jake", 3)
+    ql = que(l)
+    assert ql == ["Constantine", "Nick", "Henry", "Mark", "Jake"]
+
     l.insert("James", 0)
+    ql = que(l)
+    assert ql == ["James", "Constantine", "Nick", "Henry", "Mark", "Jake"]

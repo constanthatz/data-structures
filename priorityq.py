314b14394010f6c08f91ce76dffa359c62854187
Nick Becker <nbeck90@uw.edu>
2015-02-12 13:05:46 -0800

    Add pop() function

--- priorityq.py
+++ priorityq.py
@@ -47,22 +47,7 @@ class Priorityq(object):
                     new_element.behind, self.front = self.front, new_element
                     return
 
-    def dequeue(self):
-        ''' Remove front element from front of queue. Reassign front data
-            element and return value of dequeued element. '''
-        try:
-            val = self.front.val
-        except AttributeError:
-            ''' Mimic error message from list '''
-            raise IndexError("dequeue from empty Stack")
+    def pop(self):
+        pop_value = self.front
         self.front.behind.ahead, self.front = None, self.front.behind
-        return val
-
-    def size(self):
-        ''' Return size of queue (number of elements). '''
-        count = 0
-        current = self.back
-        while current:
-            count += 1
-            current = current.ahead
-        return count
+        return pop_value

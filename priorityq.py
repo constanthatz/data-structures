37190330396bf1143385f5346804b020a8147867
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-12 12:28:23 -0800

    rename enqueue to insert and initalize sort function.

--- priorityq.py
+++ priorityq.py
@@ -4,20 +4,21 @@ from __future__ import print_function
 
 class Element(object):
     ''' Create data element with default value and next pointer. '''
-    def __init__(self, value, ahead=None, behind=None):
+    def __init__(self, value, ahead=None, behind=None, priority=0):
         ''' Value and next pointer default to none. '''
         self.val = value
         self.ahead = ahead
         self.behind = behind
+        self.prio = priority
 
 
-class Queue(object):
+class Priorityq(object):
     ''' Create an empty queue. '''
     def __init__(self):
         self.front = None
         self.back = None
 
-    def enqueue(self, value):
+    def insert(self, value):
         ''' Add data element to the back of queue. '''
         self.back = Element(value, self.back)
 
@@ -25,6 +26,9 @@ class Queue(object):
             self.front = self.back
         else:
             self.back.ahead.behind = self.back
+
+    def __sort(self):
+        return
 
     def dequeue(self):
         ''' Remove front element from front of queue. Reassign front data

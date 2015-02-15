8040e052af8aa84d2c25b219e1753c63a7994120
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-14 18:32:42 -0800

    removed uneeded tests.

--- test_priorityq.py
+++ test_priorityq.py
@@ -22,7 +22,6 @@ def test_element_init():
 def test_prioq_init():
     l = Priorityq()
     assert l.front is None
-    assert l.back is None
 
 
 def test_priorityq_prio():
@@ -30,26 +29,22 @@ def test_priorityq_prio():
     l.insert("Nick", 1)
     ql = que(l)
     assert l.front.val == "Nick"
-    assert l.back.val == "Nick"
     assert ql == ["Nick"]
 
     l.insert("Constantine", 1)
     ql = que(l)
     assert l.front.val == "Nick"
-    assert l.back.val == "Constantine"
     assert ql == ["Constantine", "Nick"]
 
     l.insert("Mark", 2)
     ql = que(l)
     assert l.front.val == "Mark"
-    assert l.back.val == "Constantine"
     assert ql == ["Constantine", "Nick",  "Mark"]
 
     l.insert("Henry", 2)
     ql = que(l)
     assert l.front.val == "Mark"
     assert l.front.behind.val == "Henry"
-    assert l.back.val == "Constantine"
     assert ql == ["Constantine", "Nick", "Henry", "Mark"]
 
     l.insert("Jake", 3)

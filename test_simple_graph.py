2d1fd9c81ca9f17270ecef6505830cb798632091
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-16 13:19:48 -0800

    initialize graph test file.

---
+++ test_simple_graph.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+def test_init(self):
+    return
+
+
+def test_nodes():
+    return nodes
+
+
+def test_edges():
+    return edges
+
+
+def test_add_node(value):
+    return
+
+
+def test_add_edge(value1, value2):
+    return
+
+
+def test_del_node(value):
+    return
+
+
+def test_has_node(value):
+    return
+
+
+def test_neighbors(value):
+    return neighbors
+
+
+def test_adjecent(value, value2):
+    return condition

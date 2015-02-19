d74028ebd0911224f0b4b54b44173c0483534b39
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-19 12:49:45 -0800

    update queue from other branch and new tests.

--- test_simple_graph.py
+++ test_simple_graph.py
@@ -3,132 +3,168 @@ from simple_graph import Graph
 import pytest
 
 
-def test_init():
-    ''' Test graph creation. '''
-    assert Graph().graph == {}
+@pytest.fixture(scope='function')
+def empty_graph():
+    return Graph()
 
 
-def test_add_node():
-    ''' Test node adding. '''
+@pytest.fixture(scope='function')
+def non_empty_graph():
     g = Graph()
+    g.add_node("A")
+    return g
+
+
+@pytest.fixture(scope='function')
+def edges_graph():
+    g = Graph()
+    g.add_edge("A", "B")
+    return g
+
+
+def test_init(empty_graph):
+    ''' Test graph creation. '''
+    g = empty_graph
+    assert g.graph == {}
+
+
+def test_add_node_empty(empty_graph):
+    ''' Test node adding. '''
     # Add to empty graph
+    g = empty_graph
     g.add_node("A")
     assert "A" in g.graph
     assert g.graph["A"] == []
 
+
+def test_add_node_non_empty(non_empty_graph):
+    ''' Test node adding. '''
+    g = non_empty_graph
     # Add to populated graph
     g.add_node("B")
     assert "B" in g.graph
     assert g.graph["B"] == []
 
 
-def test_nodes():
+def test_nodes_empty(empty_graph):
     ''' Test node listing. '''
-    g = Graph()
+    g = empty_graph
     # Test empty graph
     assert g.nodes() == []
-    g.add_node("A")
+
+
+def test_nodes_non_empty(non_empty_graph):
+    ''' Test node listing. '''
+    g = Graph()
     g.add_node("B")
     # Test populated graph
     assert "A" and "B" in g.graph
 
 
-def test_add_edge():
-    ''' Test addinga edge to graph. '''
-    g = Graph()
+def test_add_edge_empty(empty_graph):
+    ''' Test adding an edge to graph. '''
+    g = empty_graph
     # Add to empty graph
     g.add_edge("A", "B")
     assert "A" in g.graph
     assert "B" in g.graph
     assert "B" in g.graph["A"]
 
-    # Add edge existing node
+
+def test_add_edge_existing_node(non_empty_graph):
+    ''' Test adding an edge to graph. '''
+    # Add edge from existing node
+    g = non_empty_graph
     g.add_edge("A", "C")
     assert "C" in g.graph
     assert "C" in g.graph["A"]
 
-    # Add edge non-existing node
-    g.add_edge("D", "B")
-    assert "D" in g.graph
-    assert "B" in g.graph["D"]
 
+def test_add_edge_non_existing_node(non_empty_graph):
+    ''' Test adding an edge to graph. '''
+    # Add edge from non-existing node
+    g = non_empty_graph
+    g.add_edge("C", "A")
+    assert "C" in g.graph
+    assert "A" in g.graph["C"]
+
+
+def test_add_edge_existing_nodes(non_empty_graph):
+    ''' Test addinga edge to graph. '''
     # Add edge between two existing nodes
-    g.add_edge("D", "A")
-    assert "A" in g.graph["D"]
+    g = non_empty_graph
+    g.add_node("C")
+    g.add_edge("A", "C")
+    assert "C" in g.graph["A"]
 
 
-def test_edges():
+def test_edges_empty(empty_graph):
     ''' Test edge listing. '''
-    g = Graph()
+    g = empty_graph
     # Test empty graph
     assert [] == g.edges()
-    g.add_edge("A", "B")
-    # Add to empty graph
+
+
+def test_edges_non_empty(edges_graph):
+    g = edges_graph
+    # Test non-empty graph
     assert ("A", "B") in g.edges()
 
-    # Add edge non-existing node
-    g.add_edge("A", "C")
-    assert ("A", "C") in g.edges()
 
-    # Add additional reverse edge between two nodes
-    g.add_edge("B", "A")
-    assert ("B", "A") in g.edges()
-
-    # Add edge non-existing node
-    g.add_edge("D", "B")
-    assert ("D", "B") in g.edges()
-
-    # Add node without edge
-    g.add_node("E")
-    assert ("E", ) not in g.edges()
-
-
-def test_del_edge():
+def test_del_edge_empty(empty_graph):
     ''' Test deleting an edge from graph. '''
-    g = Graph()
-
+    g = empty_graph
     # Empty graph
     with pytest.raises(KeyError):
         g.del_edge("A", "B")
 
+
+def test_del_edge_no_neighbors(non_empty_graph):
+    ''' Test deleting an edge from graph. '''
     # Node1 doesn't exit
-    g.add_node("C")
+    g = non_empty_graph
     with pytest.raises(KeyError):
-        g.del_edge("A", "C")
+        g.del_edge("C", "A")
 
     # Node2 doesn't exit
-    g.add_node("A")
     with pytest.raises(ValueError):
-        g.del_edge("A", "B")
+        g.del_edge("A", "C")
 
     # Node2 exists but isn't a neighbor
     g.add_node("B")
     with pytest.raises(ValueError):
         g.del_edge("A", "B")
 
+
+def test_del_edge(edges_graph):
+    ''' Test deleting an edge from graph. '''
+    g = edges_graph
     # Both nodes exist and one is a neighbor
-    g.add_edge("A", "D")
-    assert ("A", "D") in g.edges()
-    g.del_edge("A", "D")
-    assert ("A", "D") not in g.edges()
+    g.del_edge("A", "B")
+    assert ("A", "B") not in g.edges()
 
+
+def test_del_edge_reverse(edges_graph):
+    ''' Test deleting an edge from graph. '''
+    g = edges_graph
     # Both nodes exist and but the edge is the opposite direction
-    g.add_edge("A", "D")
-    assert ("A", "D") in g.edges()
     with pytest.raises(ValueError):
-        g.del_edge("D", "A")
-    assert ("A", "D") in g.edges()
+        g.del_edge("B", "A")
+    assert ("A", "B") in g.edges()
 
 
-def test_del_node():
+def test_del_node(empty_graph):
     ''' Test deleting a node. '''
-    g = Graph()
+    g = empty_graph
     # Empty graph
     with pytest.raises(KeyError):
         g.del_node("A")
 
+
+def test_del_node_unconnected(non_empty_graph):
+    ''' Test deleting a node. '''
+    g = non_empty_graph
     # Unconnected node
-    g.add_node("A")
     g.del_node("A")
     assert "A" not in g.graph
 
@@ -147,9 +183,9 @@ def test_del_node():
     assert ("A", "B") not in g.edges()
 
 
-def test_has_node():
+def test_has_node_empty(empty_graph):
     ''' Test if a node exists. '''
-    g = Graph()
+    g = empty_graph
     # Empty graph
     assert g.has_node("A") is False
 
@@ -159,9 +195,9 @@ def test_has_node():
     assert g.has_node("B") is False
 
 
-def test_neighbors():
+def test_neighbors_empty(empty_graph):
     ''' Test neighbor listing. '''
-    g = Graph()
+    g = empty_graph
 
     # Empty graph
     with pytest.raises(KeyError):
@@ -186,9 +222,9 @@ def test_neighbors():
     assert g.neighbors("A") == ["B", "C", "D"]
 
 
-def test_adjacent():
+def test_adjacent_empty(empty_graph):
     ''' Test if two nodes are adjacent. '''
-    g = Graph()
+    g = empty_graph
 
     # Test empty graph
     with pytest.raises(KeyError):

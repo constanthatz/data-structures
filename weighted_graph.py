0bb1cda1d4d292c1c7b6017942d417b454b3c7b4
Nick Becker <nbeck90@uw.edu>
2015-03-02 15:01:01 -0800

    Changed data structure

--- weighted_graph.py
+++ weighted_graph.py
@@ -29,16 +29,14 @@ class Graph(object):
             We are using dictionaries to store neighbors and weights so we
             don't have to loop through lists to remove neighbros and weights.
             '''
-        self.graph.setdefault(node, {'neighbors': [], 'weights': []})
+        self.graph.setdefault(node, {})
 
     def add_edge(self, node1, node2, weight=0):
         ''' Add an edge to the graph. '''
         # Add node1 to graph
-        self.graph.setdefault(node1, {'neighbors': [], 'weights': []})
+        self.graph.setdefault(node1, {})
         # Add edge
-        self.graph[node1]['neighbors'].append(node2)
-        self.graph[node1]['weights'].append(weight)
-
+        self.graph[node1].update({node2: weight})
         # Check if node1 is in the graph
         if node2 not in self.graph:
             self.add_node(node2)
@@ -46,14 +44,9 @@ class Graph(object):
     def del_edge(self, node1, node2):
         ''' Delete an edge to the graph. '''
         try:
-            # Check if node1 exists and node2 is a neighbor and delete the edge
-            idx = self.graph[node1]['neighbors'].index(node2)
-            self.graph[node1]['neighbors'].remove(node2)
-            del self.graph[node1]['weights'][idx]
+            del self.graph[node1][node2]
         except KeyError:
-            raise KeyError('node {} not in graph'.format(node1))
-        except ValueError:
-            raise ValueError('node {} not a neighbor'.format(node2))
+            raise KeyError('One of the given nodes not in graph'.format(node1))
 
     def del_node(self, node):
         ''' Delete a node from the graph. '''
@@ -62,10 +55,8 @@ class Graph(object):
             # Delete the node from any neighbor lists it is in.
             for value in self.graph.itervalues():
                     try:
-                        idx = value['neighbors'].index(node)
-                        value['neighbors'].remove(node)
-                        del value['weights'][idx]
-                    except ValueError:
+                        del value[node]
+                    except KeyError:
                         pass
         except KeyError:
             raise KeyError('{} not in graph'.format(node))
@@ -77,14 +68,14 @@ class Graph(object):
     def neighbors(self, node):
         ''' Return the neighbors of a node. '''
         try:
-            return self.graph[node]['neighbors']
+            return self.graph[node].keys()
         except KeyError:
             raise KeyError('{} not in graph'.format(node))
 
     def adjacent(self, node1, node2):
         ''' Check is if two nodes have an edge connecting them. '''
         try:
-            return node2 in self.graph[node1]['neighbors']
+            return node2 in self.graph[node1].keys()
         except KeyError:
             raise KeyError('{} not in graph'.format(node1))
 
@@ -127,3 +118,15 @@ class Graph(object):
                     path.append(neighbor)
                     queue.enqueue(neighbor)
         return path
+
+if __name__ == "__main__":
+    g = Graph()
+    g.add_node(3)
+    print(g.nodes())
+    g.add_edge(4, 8)
+    print(g.nodes())
+    print(g.neighbors(4))
+    print(g.adjacent(4, 8))
+    g.del_edge(4, 8)
+    print(g.neighbors(4))
+    print(g.adjacent(4, 8))
